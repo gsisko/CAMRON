4f198490d80b5ac956167455c411c46ccdb2ba33
Gabriel Solomon Isko <gsisko@CCCWORK3.WPI.EDU>
2012-12-05 13:18:29 -0500

    force sense and PID code implemented

---
+++ StateLibrary/RobotState.cpp
@@ -0,0 +1,36 @@
+#include RobotState.h
+
+RobotState::RobotState(int initialX, initialY)
+{
+	posX = initialX;
+	posY = initialY;
+	SpeedX = 0;
+	SpeedY = 0;
+	GoalPositionX = 0;
+	GoalPositionY = 0;
+	GoalForce = 0;
+	P = 0; I = 0; D = 0;
+	for(int i=0; i<SUM_SIZE;i++)
+	{
+		sumnation[i] = 0;
+	}
+	sumposition = 0;
+}
+ 
+float RobotState::ForceSense(); //these funcitions try to attain the goal force and position of the robot, returns error
+{
+	sumposition += 1;
+	if(sumposition > SUM_SIZE)
+		sumposition = 0;
+	sumnation[sumposition] = GoalForce - EraserForce; 
+	diff = sumnation[sumposition] - sumnation[((sumposition - 1) < 0) ?SUM_SIZE : (sumposition - 1)];
+	sum = sumnation[sumposition];
+	sum -= sumnation[((sumposition+1) < SUM_SIZE)? (sumposition+1) : 0];
+	
+	float ScrewOut = (sumnation[sumposition]*P)+(sum*I)-(diff*D);
+	//adjust ScrewSpeed, make sure value is bounded in the future
+	ScrewSpeed = max(0, min(ScewOut, 255); 
+	return sumnation[sumposition]
+}
+
+
--- StateLibrary/RobotState.h
+++ StateLibrary/RobotState.h
@@ -1,27 +1,34 @@
+#define SUM_SIZE
+
+
 class RobotState()
 {
 	private: 
-	//Movement Variables:
-	 int PosX; 	 //X and Y position on the whiteboard. origin is at starting position, in Encoder ticks
-	 int PosY;      
-	 char SpeedX;
-	 char SpeedY;
-	 
-	 
+		//Movement Variables:
+		int PosX; 	 //X and Y position on the whiteboard. origin is at starting position, in Encoder ticks
+		int PosY;      
+		char SpeedX;
+		char SpeedY;
 
-	//Erasing Variables
-	 float EraserForceLeft;	//force on the Eracser, in newtons
-	 float EraserForceRight;
-	 char ScrewRight;  //speeds of lead screw chnanges, as output to vex motor contorollers 
-	 char ScrewLeft;
-	 //There might need to be varibles here that are PID related, however they will probably be static variables in the PID functionjjj
-        public:
-	 RobotState(float, int); //takes the initial goal force and position
-	 void UpdateX(int); //adds buffered Encoder ticks into current position
-	 void UpdateY(int);
-	 float GoalForce; //we want the eraser to have, each side of the eraser should be at half this force
-         int GoalPosition;
-	 float ForceSense(); //these funcitions try to attain the goal force and position of the robot.
+
+
+		//Erasing Variables
+		float EraserForce;	//force on the Eracser, in newtons
+		char ScewSpeed;
+		//PID Variables, should really make a PID class
+		float sum;
+		float diff;
+		float sumnation[SUM_SIZE-1];
+		int sumposition;
+		//There might need to be varibles here that are PID related, however they will probably be static variables in the PID functionjjj
+	public:
+		RobotState(int, int); //takes the initial position
+		void UpdateX(int); //adds buffered Encoder ticks into current position
+		void UpdateY(int);
+		float GoalForce; //we want the eraser to have, each side of the eraser should be at half this force
+		float P,I,D;
+		int GoalPositionX, GoalPositionY;
+	 float ForceSense(); //these funcitions try to attain the goal force and position of the robot, returns error
 	 int Move(); 
 	  
 
