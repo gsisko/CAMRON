8d515aef977109bcbf1e888d3fc532d615b11e3c
Gabriel Isko <gabestripes@gmail.com>
2012-12-07 02:13:52 -0500

    last code of the night

--- StateLibrary/PID.cpp
+++ StateLibrary/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+
 template <class T>
 PIDControl<T>::PIDControl(T _P,T _I,T _D, T _desired)
 {
@@ -14,20 +15,30 @@ PIDControl<T>::PIDControl(T _P,T _I,T _D, T _desired)
 		sumnation[i] = 0;
 	}
 	sumposition = 0;
+        sum = 0;
+        diff = 0;
 }
 
 
 template <class T>
 T PIDControl<T>::operate(T *input, T* output)
 {
-	sumposition += 1;
-	if(sumposition > SUM_SIZE)
+	diff = sumnation[sumposition];
+        sumposition += 1;
+	if(sumposition >= SUM_SIZE)
 		sumposition = 0;
-	sumnation[sumposition] = desired - *input; 
-	diff = sumnation[sumposition] - sumnation[((sumposition - 1) < 0) ?SUM_SIZE : (sumposition - 1)];
-	sum += sumnation[sumposition];
-	sum -= sumnation[((sumposition+1) < SUM_SIZE)? (sumposition+1) : 0];
-
+        sumnation[sumposition] = desired - *input;
+        diff = sumnation[sumposition] - diff; 
+	int i = SUM_SIZE;
+        sum = 0;
+        for(i = 0; i < SUM_SIZE; i++)
+          {
+            //Serial.println(sumnation[i]);
+            sum += sumnation[i];
+          }
+        
+        Serial.print(sum);
+        Serial.print("  .   ");
 	return *output = (sumnation[sumposition]*P)+(sum*I)-(diff*D);
         
 }
