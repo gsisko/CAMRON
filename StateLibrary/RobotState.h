ce458b75e120f6a39a02fa970beaf212cc788498
Gabriel Isko <gabestripes@gmail.com>
2012-12-07 00:57:24 -0500

    now uses init function

--- StateLibrary/RobotState.h
+++ StateLibrary/RobotState.h
@@ -1,6 +1,5 @@
 #define SUM_SIZE 10
 
-
 class RobotState
 {
 private: 
@@ -13,10 +12,11 @@ private:
 
 
   //Erasing Variables
-  float EraserForce;	//force on the Eracser, in newtons
-  float ScrewSpeed;
+  
   //PID Variables, should really make a PID class
   //PID class instead!
+  float EraserForce;	//force on the Eraser
+  float ScrewSpeed;
   PIDControl<float> *controller;
   /*float sum;
   float diff;
@@ -25,21 +25,23 @@ private:
    */
   //There might need to be varibles here that are PID related, however they will probably be static variables in the PID function
 public:
-  RobotState(int, int, float, float, float, float); //takes the initial position
+ 
+  RobotState();
+  void Init(int, int, float, float, float, float); //takes the initial position
   void UpdateX(int); //adds buffered Encoder ticks into current position
   void UpdateY(int);
   /*float GoalForce; //we want the eraser to have, each side of the eraser should be at half this force
   float P,I,D;*/
   int GoalPositionX, GoalPositionY;
   float ForceSense(); //these funcitions try to attain the goal force and position of the robot, returns error
-  int Move(); 
-
+  int Move();
   void setPID(float, float, float, float);
   void setP(float);
   void setI(float);
   void setD(float);
   void setDES(float);
+  float* checkErase();
+
 
 
 };
-
