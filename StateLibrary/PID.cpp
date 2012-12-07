897bf741116e3f85e01caf7f939437636da884b0
Gabriel Isko <gabestripes@gmail.com>
2012-12-07 00:53:59 -0500

    operate now takes input and output

--- StateLibrary/PID.cpp
+++ StateLibrary/PID.cpp
@@ -1,10 +1,10 @@
 #include "PID.h"
-
 template <class T>
-PIDControl<T>::PIDControl(T* _input, T* _output, T _P,T _I,T _D, T _desired)
+PIDControl<T>::PIDControl(T _P,T _I,T _D, T _desired)
 {
-	input = _input;
-	output = _output;
+	
+	
+        
 	desired = _desired;
 	P = _P; 
 	I = _I; 
@@ -18,16 +18,17 @@ PIDControl<T>::PIDControl(T* _input, T* _output, T _P,T _I,T _D, T _desired)
 
 
 template <class T>
-T PIDControl<T>::operate()
+T PIDControl<T>::operate(T *input, T* output)
 {
 	sumposition += 1;
 	if(sumposition > SUM_SIZE)
 		sumposition = 0;
-	sumnation[sumposition] = *desired - *input; 
+	sumnation[sumposition] = desired - *input; 
 	diff = sumnation[sumposition] - sumnation[((sumposition - 1) < 0) ?SUM_SIZE : (sumposition - 1)];
-	sum = sumnation[sumposition];
+	sum += sumnation[sumposition];
 	sum -= sumnation[((sumposition+1) < SUM_SIZE)? (sumposition+1) : 0];
 
 	return *output = (sumnation[sumposition]*P)+(sum*I)-(diff*D);
-
+        
 }
+
