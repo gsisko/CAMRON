423f7d1f90ad2453b42c6c51bea10db1be525cd4
Gabriel Isko <gabestripes@gmail.com>
2012-12-07 00:55:30 -0500

    now implements correct operate

--- StateLibrary/RobotState.cpp
+++ StateLibrary/RobotState.cpp
@@ -1,38 +1,17 @@
 #include RobotState.h
 
-//P is initially .42 because this is the estimated 1 to 1 mapping of the input to the output across their respect ranges
-//this estimation is based on that we are mapping 60% of the range of a 10 bit value to an 8 bit value.
-RobotState::RobotState(int initialX, int initialY, float _P = .42, float _I = 0, float _D = 0, float _desired = 1)
-{
-	PosX = initialX;
-	PosY = initialY;
-	SpeedX = 0;
-	SpeedY = 0;
-	GoalPositionX = 0;
-	GoalPositionY = 0;
-	controller = new PIDControl<float>(&EraserForce, &ScrewSpeed, _P, _I, _D, _desired);
-	/*GoalForce = 0;
-	  P = 0; 
-	  I = 0; 
-	  D = 0;
-	  for(int i=0; i<SUM_SIZE;i++)
-	  {
-	  sumnation[i] = 0;
-	  }
-	  sumposition = 0;
-	 */
-}
 
 float RobotState::ForceSense() //these funcitions try to attain the goal force and position of the robot, returns error
 {
-        controller->operate();
+        EraserForce = (float)analogRead(0);
+        return controller->operate(&EraserForce, &ScrewSpeed);
 	/*
 	   sumposition += 1;
 	   if(sumposition > SUM_SIZE)
 	   sumposition = 0;
 	   sumnation[sumposition] = GoalForce - EraserForce; 
 	   diff = sumnation[sumposition] - sumnation[((sumposition - 1) < 0) ?SUM_SIZE : (sumposition - 1)];
-	   sum = sumnation[sumposition];
+	   sum += sumnation[sumposition];
 	   sum -= sumnation[((sumposition+1) < SUM_SIZE)? (sumposition+1) : 0];
 	 
 
@@ -40,9 +19,10 @@ float RobotState::ForceSense() //these funcitions try to attain the goal force a
 	 */
 
   //adjust ScrewSpeed, make sure value is bounded in the future
-  ScrewSpeed = max(0, min(ScrewSpeed, 255)); 
+  //ScrewSpeed = max(0, min(ScrewSpeed, 255)); 
   return ScrewSpeed;
 }
+
 void RobotState::setPID(float _P, float _I, float _D, float _desired)
 {
   controller->P = _P;
